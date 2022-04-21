d1eb671931bd6d13d69e716753e7eb8c46e24a4d
pp3585 <pooja.prasa@gmail.com>
2022-04-21 23:59:12 +0000

    Added database connect, config

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const msg = "Hello";
-console.log(msg, "testing changes");
+//Add modules required
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDb = require("./config/database");
+
+//Initialize express
+const app = express();
+
+//Read config file
+dotenv.config({path: '.config/config.env'});
+
+//Connect database
+connectDb();
+
+//PORT is read from the config file
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
